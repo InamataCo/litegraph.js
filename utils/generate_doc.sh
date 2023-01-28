85b5bebc8c6895cef61617aaa30f5a3626580b60
Moritz Ulmer <moritz.ulmer@posteo.de>
2023-01-28 22:39:28 +0100

    Move docs for Github pages

--- utils/generate_doc.sh
+++ utils/generate_doc.sh
@@ -3,5 +3,5 @@
 # For migration to JSDoc (YUIDoc was deprecated in 2014): https://github.com/pnstickne/yuidoc-to-jsdoc
 
 cd "$(dirname "$0")" || exit
-rm -rf ../doc/*
-yuidoc ../src -o ../doc
+rm -rf ../docs/*
+yuidoc ../src -o ../docs
