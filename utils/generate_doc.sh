1e6a32d77e8b9b585697f54f00a36048713c0d93
Moritz Ulmer <moritz.ulmer@posteo.de>
2023-01-28 22:27:56 +0100

    Update docs and add to release

--- utils/generate_doc.sh
+++ utils/generate_doc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# For migration to JSDoc (YUIDoc was deprecated in 2014): https://github.com/pnstickne/yuidoc-to-jsdoc
+
 cd "$(dirname "$0")" || exit
 rm -rf ../doc/*
 yuidoc ../src -o ../doc
