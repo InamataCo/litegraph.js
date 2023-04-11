688f80ffb6d27db608099b84f414daa71a09f8c4
Moritz Ulmer <moritz.ulmer@posteo.de>
2023-04-11 10:39:12 +0200

    Run tests on multiple Node.js versions

--- utils/test.sh
+++ utils/test.sh
@@ -11,6 +11,6 @@ NODE_VERSIONS=("lts/erbium" "lts/fermium" "lts/gallium" "lts/hydrogen")
 
 for NODE_VERSION in "${NODE_VERSIONS[@]}"; do
   nvm install "$NODE_VERSION"
-  nvm exec "$NODE_VERSION" npm install
+  nvm exec "$NODE_VERSION" npm install --include=dev
   nvm exec "$NODE_VERSION" npm test
 done
